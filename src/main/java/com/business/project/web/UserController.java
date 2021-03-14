14360c15f9555187f3045198016232a98b9e1bca
Pavel Naydenov <70015071+Pavkata00@users.noreply.github.com>
2021-03-14 15:41:46 +0200

    Adding controller and edit html

---
+++ src/main/java/com/business/project/web/UserController.java
@@ -0,0 +1,22 @@
+package com.business.project.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/users")
+public class UserController {
+
+    @GetMapping("/login")
+    private String login() {
+
+        return "login";
+    }
+
+    @GetMapping("/register")
+    private String register() {
+
+        return "register";
+    }
+}
