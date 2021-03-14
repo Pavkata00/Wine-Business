62ab5ddb5565ddc359988df5e89cf740ec01af81
Pavel Naydenov <70015071+Pavkata00@users.noreply.github.com>
2021-03-14 16:29:28 +0200

    Editing pics.

--- src/main/java/com/business/project/config/AppSecurityConfig.java
+++ src/main/java/com/business/project/config/AppSecurityConfig.java
@@ -29,7 +29,7 @@ public class AppSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests().
-                antMatchers("/","/index","/home").permitAll().
+                antMatchers("/","/index","/home","/register", "/login").permitAll().
                 antMatchers("/js/**", "/css/**", "/img/**").permitAll();
     }
 }
